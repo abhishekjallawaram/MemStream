ab1abda6b5b94e3e8205a02f352567bdd2f69645
abhishekjallawaram <jallawaramabhishek@gmail.com>
2023-06-07 17:51:01 -0400

    Update memstream-ede.py

--- code/memstream-ede.py
+++ code/memstream-ede.py
@@ -31,28 +31,64 @@ start_time = time.time()
 import argparse
 
 parser = argparse.ArgumentParser()
+
+# --dataset: Specifies the dataset to be used, with 'NSL' as the default value.
 parser.add_argument('--dataset', default='NSL')
+
+# --beta: Specifies the value of beta as a floating-point number, with 0.1 as the default value.
 parser.add_argument('--beta', type=float, default=0.1)
+
+# --dev: Specifies the device to be used, with 'cuda:0' as the default value.
 parser.add_argument("--dev", help="device", default="cuda:0")
+
+# --epochs: Specifies the number of epochs for the autoencoder, with 5000 as the default value.
 parser.add_argument("--epochs", type=int, help="number of epochs for ae", default=5000)
+
+# --lr: Specifies the learning rate for the optimizer, with 1e-2 as the default value.
 parser.add_argument("--lr", type=float, help="learning rate", default=1e-2)
+
+# --memlen: Specifies the size of the memory, with 2048 as the default value.
 parser.add_argument("--memlen", type=int, help="size of memory", default=2048)
+
+# --seed: Specifies the random seed to be used, with 0 as the default value.
 parser.add_argument("--seed", type=int, help="random seed", default=0)
-parser.add_argument("--act_fn",help="Activation fucntion", default="Tanh")
-parser.add_argument("--RQ1", help="Hypertune best metrics : Default False", default=False)
-parser.add_argument("--RQ2", help="Effect of Activation Functions : Default False",default=False)
-parser.add_argument("--RQ3", help="Memory Poisoning Prevention Analysis",default=False)
-parser.add_argument("--RQ4", help="Concept Drift",default=False)
-parser.add_argument("--RQ5", help="Impact of Memory",default=False)
-parser.add_argument("--sp",help="Sampling Method", default="rand")
 
+# --act_fn: Specifies the activation function to be used, with 'Tanh' as the default value.
+parser.add_argument("--act_fn", help="Activation function", default="Tanh")
+
+# --RQ1: Specifies whether to execute RQ1 (Hypertune best metrics), with False as the default value.
+parser.add_argument("--RQ1", help="Hypertune best metrics: Default False", default=False)
+
+# --RQ2: Specifies whether to execute RQ2 (Effect of Activation Functions), with False as the default value.
+parser.add_argument("--RQ2", help="Effect of Activation Functions: Default False", default=False)
+
+# --RQ3: Specifies whether to execute RQ3 (Memory Poisoning Prevention Analysis), with False as the default value.
+parser.add_argument("--RQ3", help="Memory Poisoning Prevention Analysis", default=False)
+
+# --RQ4: Specifies whether to execute RQ4 (Concept Drift), with False as the default value.
+parser.add_argument("--RQ4", help="Concept Drift", default=False)
+
+# --RQ5: Specifies whether to execute RQ5 (Impact of Memory), with False as the default value.
+parser.add_argument("--RQ5", help="Impact of Memory", default=False)
+
+# --sp: Specifies the sampling method to be used, with 'rand' as the default value.
+parser.add_argument("--sp", help="Sampling Method", default="rand")
+
+
+# Parsing the command-line arguments and storing them in the `args` object.
 args = parser.parse_args()
 
+# Parsing the command-line arguments and storing them in the `args` object.
 args = parser.parse_args()
 
+# Setting the random seed for reproducibility based on the value specified in the command-line arguments.
 torch.manual_seed(args.seed)
+
+# Initializing variables to hold the filenames for the dataset files.
 nfile = None
 lfile = None
+
+# Selecting the appropriate dataset based on the value of `args.dataset`.
 if args.dataset == 'NSL':
     nfile = 'nsl.txt'
     lfile = 'nsllabel.txt'
@@ -69,17 +105,26 @@ elif args.dataset == 'SYN':
     nfile = 'syn.txt'
     lfile = 'synlabel.txt'
 else:
+    # Loading the dataset from a .mat file with the name specified in `args.dataset`.
     df = scipy.io.loadmat(args.dataset+".mat")
     numeric = torch.FloatTensor(df['X'])
     labels = (df['y']).astype(float).reshape(-1)
 
+# Setting the device to be used for computation based on the value specified in the command-line arguments.
 device = torch.device(args.dev)
 
 
-from sklearn.preprocessing import MinMaxScaler
+
+#from sklearn.preprocessing import MinMaxScaler
 
 class MemStream(nn.Module):
     def __init__(self, in_dim, params, act_fn):
+        """
+        Initializes the MemStream module with the given input dimension (in_dim),
+        parameters (params), and activation function (act_fn). It sets up various
+        attributes such as memory length, maximum threshold, memory tensors,
+        network layers, and loss functions.
+        """
         super(MemStream, self).__init__()
         self.params = params
         self.in_dim = in_dim
@@ -137,6 +182,12 @@ class MemStream(nn.Module):
         self.count = 0
 
     def train_autoencoder(self, data, epochs):
+        """
+        Trains the autoencoder part of the network using the given data for
+        the specified number of epochs. It normalizes the data, performs
+        forward and backward passes, and updates the parameters using the
+        Adam optimizer.
+        """
         self.mean, self.std = self.mem_data.mean(0), self.mem_data.std(0)
         new = (data - self.mean) / self.std
         new[:, self.std == 0] = 0
@@ -162,6 +213,11 @@ class MemStream(nn.Module):
 
 
     def update_memory(self, output_loss, encoder_output, data):
+        """
+        Updates the memory of the module based on the output_loss (a scalar value),
+        encoder_output, and data. If the output_loss is below the maximum threshold,
+        it stores the encoder_output and data in the memory tensor.
+        """
         if output_loss <= self.max_thres:
             least_used_pos = self.count % self.memory_len
             if least_used_pos < self.memory.size(0):
@@ -174,6 +230,10 @@ class MemStream(nn.Module):
 
 
     def initialize_memory(self, x):
+        """
+        Initializes the memory of the module using the input x. It normalizes the input,
+        passes it through the encoder, and sets the memory tensor accordingly.
+        """
         mean, std = self.mem_data.mean(0), self.mem_data.std(0)
         new = (x - mean) / std
         new[:, std == 0] = 0
@@ -182,6 +242,11 @@ class MemStream(nn.Module):
         self.mem_data = x.to(device)
 
     def forward(self, x):
+        """
+        Performs the forward pass of the module with the given input x. It normalizes
+        the input, passes it through the encoder and decoder, calculates the
+        reconstruction and encoding losses, updates the memory, and returns the total loss.
+        """
         new = (x - self.mean) / self.std
         new[:, self.std == 0] = 0
         encoded1 = self.encoder1(new + 0.001 * torch.randn_like(new).to(device))
@@ -206,12 +271,22 @@ class MemStream(nn.Module):
 
 class EnsembleMemStream():
     def __init__(self, num_models, params, act_fn):
+        """
+        Initializes the EnsembleMemStream class with the specified number of models,
+        parameters (params), and activation function (act_fn). It creates a list of
+        MemStream models and stores them in the 'models' attribute.
+        """
         self.num_models = num_models
         self.models = []
         for i in range(num_models):
             self.models.append(MemStream(params['in_dim'],params=params, act_fn=act_fn))
             
     def train(self, numeric, labels, epochs):
+        """
+        Trains each model in the ensemble using the given numeric data and labels for
+        the specified number of epochs. It initializes the model's memory, performs
+        training, and evaluates the model's performance using ROC-AUC score.
+        """
         for i in range(self.num_models):
             print(f"Training Model {i+1}/{self.num_models}")
             np.random.seed(i)
@@ -236,6 +311,10 @@ class EnsembleMemStream():
             print(f"Model {i+1} ROC-AUC: {auc}")
             
     def predict(self, numeric):
+        """
+        Generates predictions using the ensemble of models for the given numeric data.
+        It calculates the average score from each model and returns the ensemble scores.
+        """
         ensemble_scores = np.zeros(len(numeric))
         for model in self.models:
             data_loader = DataLoader(numeric, batch_size=params['batch_size'])
@@ -251,26 +330,38 @@ class EnsembleMemStream():
 
         
 if args.dataset in ['KDD', 'NSL', 'UNSW', 'DOS']:
-    numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter = ','))
+    # Loading the numeric and label data from the corresponding files based on the selected dataset.
+    numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter=','))
     labels = np.loadtxt(lfile, delimiter=',')
     act_fn = nn.Tanh()
 # else:
 #     act_fn = nn.LogSoftmax(dim=1)
 
-    
+# Setting the activation function to nn.Tanh() for the selected datasets.
 act_fn = nn.Tanh()
-    
+
+# Modifying the labels for the 'KDD' dataset.
 if args.dataset == 'KDD':
     labels = 1 - labels
+
+# Setting the random seed based on the value specified in the command-line arguments.
 torch.manual_seed(args.seed)
+
+# Setting the memory length and other parameters for the MemStream module.
 N = args.memlen
 params = {
-          'beta': args.beta, 'memory_len': N, 'batch_size':1, 'lr':args.lr , 'in_dim' : numeric[0].shape[0]
-         }
+    'beta': args.beta,
+    'memory_len': N,
+    'batch_size': 1,
+    'lr': args.lr,
+    'in_dim': numeric[0].shape[0]
+}
 
+# Modifying the numeric and labels for the 'SYN' dataset.
 if args.dataset == 'SYN':
-    numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter = ',')).reshape(-1, 1)
+    numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter=',')).reshape(-1, 1)
     labels = np.loadtxt(lfile, delimiter=',')
+
 
 
 # ensemble_model = EnsembleMemStream(num_models=20, params=params, act_fn=act_fn)
@@ -285,7 +376,7 @@ if args.dataset == 'SYN':
 # print("Ensemble ROC-AUC:", auc)
     
 
-
+#If the sampling method is Random Sampling
 if args.sp == 'rand':    
 
     if args.dataset in ['KDD', 'NSL', 'UNSW', 'DOS']:
@@ -329,7 +420,7 @@ if args.sp == 'rand':
     
 
 
-    
+#If the sampling method is Stratified Sampling    
 elif args.sp =='str':
     if args.dataset in ['KDD', 'NSL', 'UNSW', 'DOS']:
         numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter=','))
@@ -378,6 +469,7 @@ elif args.sp =='str':
     print(f"Execution time: {time.time()-start_time:.2f} seconds")
     print("ROC-AUC", auc)
 
+#If the sampling method is Random OverSampling
 elif args.sp == 'ovr':
     if args.dataset in ['KDD', 'NSL', 'UNSW', 'DOS']:
         numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter = ','))
@@ -422,6 +514,7 @@ elif args.sp == 'ovr':
     print(f"Execution time: {time.time()-start_time:.2f} seconds")
     print("ROC-AUC", auc)
 
+#If the sampling method is SMOTE
 elif args.sp == 'sm':
     if args.dataset in ['KDD', 'NSL', 'UNSW', 'DOS']:
         numeric = torch.FloatTensor(np.loadtxt(nfile, delimiter = ','))
@@ -465,7 +558,8 @@ elif args.sp == 'sm':
     auc = metrics.roc_auc_score(labels_resampled, scores)
     print(f"Execution time: {time.time()-start_time:.2f} seconds")
     print("ROC-AUC", auc)
-
+    
+#If the sampling method is Ensembled Approach
 elif args.sp == 'ens_m':
 
     ensemble_model = EnsembleMemStream(num_models=5, params=params, act_fn=act_fn)
